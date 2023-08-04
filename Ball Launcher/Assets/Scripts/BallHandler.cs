14172a04c2394be4afe8182afd79af8d5464de36
Kayle Maximus <kaylemaximus@gmail.com>
2023-08-04 22:13:05 +0700

    Adding Launching ball mechanic

--- Ball Launcher/Assets/Scripts/BallHandler.cs
+++ Ball Launcher/Assets/Scripts/BallHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,17 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D _currentBallRigidbody;
+    [SerializeField] private GameObject _ballPrefab;
+    [SerializeField] private Rigidbody2D _pivot;
+    [SerializeField] private float _detachTime;
+    [SerializeField] private float _respawnDelay;
+
+
+    private Rigidbody2D _currentBallRigidbody;
+    private SpringJoint2D _currentBallSprintJoint;
 
     private Camera _mainCamera;
+    private Boolean _isDragging;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +28,41 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!Touchscreen.current.primaryTouch.press.isPressed){
-            _currentBallRigidbody.isKinematic = false;
+        //Drow the ball 
+        if (_currentBallRigidbody == null) return;
+
+        //Release The Ball
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            if (_isDragging)
+            {
+                LaunchBall();
+            }
+            _isDragging = false;
             return;
         }
+
+        //Hold The Ball
+        _isDragging = true;
         _currentBallRigidbody.isKinematic = true;
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
         _currentBallRigidbody.position = worldPosition;
-        
+
+    }
+
+    private void LaunchBall()
+    {
+        _currentBallRigidbody.isKinematic = false;
+        _currentBallRigidbody = null;
+
+        // Invoke("DetachBall", _detachTime);
+        Invoke(nameof(DetachBall), _detachTime);
+    }
+
+    private void DetachBall()
+    {
+        _currentBallSprintJoint.enabled = false;
+        _currentBallSprintJoint = null;
     }
 }
